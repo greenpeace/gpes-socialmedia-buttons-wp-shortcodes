56d9189866b75fd4822279a9aa8c13d35eee6d36
Osvaldo Gago <opgago@yahoo.com>
2018-11-26 14:11:25 +0100

    Format and stuff

--- simple-socialmedia-buttons/js/shortcode-simple-socialmedia-buttons.js
+++ simple-socialmedia-buttons/js/shortcode-simple-socialmedia-buttons.js
@@ -1,7 +1,7 @@
 console.log("Shortcode Timber plugin javascript loaded");
 
 document.addEventListener("DOMContentLoaded", function(){
-
+    
     var analyticsEvent = function(category, action, label) {
       if (typeof(ga) == "function") {
         ga('send', 'event', category, action, label);
